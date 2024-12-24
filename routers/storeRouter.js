2fdf64aed8430bdb0065f8ad5a27d50bdc1f2a21
shubham bhavsar <shubhambhavsar5566@gmail.com>
2024-12-24 11:26:06 +0530

    basic curd is completed

--- routers/storeRouter.js
+++ routers/storeRouter.js
@@ -2,6 +2,11 @@ const express = require("express");
 const storeController = require('./../controllers/storeController')
 const storeRouter = express.Router();
 
-storeRouter.get("/", storeController.getHome);
+storeRouter.get("/", storeController.getIndex);
+storeRouter.get("/homes", storeController.getHomes);
+storeRouter.get("/homes/:homeId", storeController.getHomeDetails);
+storeRouter.get("/favourites", storeController.getFavourites);
+storeRouter.post("/favourites", storeController.postAddFavourites);
+storeRouter.post("/favourites/delete/:homeId", storeController.postRemoveFavourite);
 
 module.exports = storeRouter;
